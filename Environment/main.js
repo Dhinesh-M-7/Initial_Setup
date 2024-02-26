e9ef6aaf616039393be2e33f5b3f4baa505cff5a
Dhinesh-M-7 <dhini3322@gmail.com>
2024-02-26 12:51:40 +0530

    created a folder for images

--- Environment/main.js
+++ Environment/main.js
@@ -3,71 +3,71 @@ import * as BABYLON from "@babylonjs/core";
 const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.Engine(canvas);
 
-async function  createScene(){
+async function createScene() {
   const scene = new BABYLON.Scene(engine);
 
   const havokInstance = await HavokPhysics();
   const havokPlugin = new BABYLON.HavokPlugin(true, havokInstance);
 
-
-  const camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0, 10, -75));
+  const camera = new BABYLON.UniversalCamera(
+    "camera",
+    new BABYLON.Vector3(0, 10, -75)
+  );
   camera.setTarget(BABYLON.Vector3.Zero());
   camera.attachControl(canvas, true);
 
-  const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
+  const light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0)
+  );
   light.intensity = 0.7;
 
   createEnvironment();
 
-
   scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), havokPlugin);
-
 
   //const groundAggregate = new BABYLON.PhysicsAggregate(ground, BABYLON.PhysicsShapeType.BOX, {mass: 0});
 
   return scene;
-};
-
+}
 
 const createEnvironment = () => {
   const ground = BABYLON.MeshBuilder.CreateGround("ground", {
     width: 100,
-    height: 150
-  })
+    height: 200,
+  });
 
   const leftWall = new BABYLON.MeshBuilder.CreatePlane("plane", {
     height: 50,
-    width: 150
-  })
+    width: 200,
+  });
   leftWall.position.x = -50;
-  leftWall.position.y = 25
+  leftWall.position.y = 25;
   leftWall.rotation.y = -Math.PI / 2;
 
   const rightWall = new BABYLON.MeshBuilder.CreatePlane("plane", {
     height: 50,
-    width: 150
-  })
+    width: 200,
+  });
   rightWall.position.x = 50;
   rightWall.position.y = 25;
   rightWall.rotation.y = Math.PI / 2;
 
   const backWall = new BABYLON.MeshBuilder.CreatePlane("plane", {
     height: 50,
-    width: 100
-  })
+    width: 100,
+  });
   backWall.position.y = 25;
-  backWall.position.z = 75
-}
-
-
+  backWall.position.z = 100;
+};
 
 createScene().then((scene) => {
-  engine.runRenderLoop(function() {
-    if(scene) {
+  engine.runRenderLoop(function () {
+    if (scene) {
       scene.render();
     }
-  })
-})
-window.addEventListener('resize', function(){
+  });
+});
+window.addEventListener("resize", function () {
   engine.resize();
-})
+});
