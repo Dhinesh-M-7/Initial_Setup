6250dc8612a29ff91152fb37c436b7ccd7de84dd
karthik156kk <karthik582tn@gmail.com>
2024-02-27 13:21:46 +0530

    sample test

--- Environment/main.js
+++ Environment/main.js
@@ -59,11 +59,14 @@ async function createScene() {
     new BABYLON.Vector3(0, 0.5, 86),
   ];
 
-  pinPositions.map(function (position, idx) {
+  const setPins = pinPositions.map(function (position, idx) {
     const pin = new BABYLON.InstancedMesh("pin-" + idx, bowlingPin);
     pin.position = position;
+    return pin;
   });
 
+  // Create a new instance of StartGame with generalPins -- need gui to be added
+  const game = new StartGame(setPins, scene);
   return scene;
 }
 
@@ -164,6 +167,7 @@ const createEnvironment = () => {
   backWall4.position.z = 100;
 };
 
+
 createScene().then((scene) => {
   engine.runRenderLoop(function () {
     if (scene) {
@@ -174,3 +178,83 @@ createScene().then((scene) => {
 window.addEventListener("resize", function () {
   engine.resize();
 });
+
+
+class StartGame {
+  constructor(generalPins, scene) {
+    this.frames = [];
+    this.currentFrameIndex = 0;
+    this.currentRoll = 1;
+    this.ballIsRolled = false;
+    this.generalPins = generalPins;
+    this.pinsArray = [];
+    this.initializeFrames();
+    this.initializePinsArray();
+    this.setupEventListeners(scene);
+  }
+
+  // Function to initialize frames
+  initializeFrames() {
+    for (let i = 0; i < 10; i++) {
+      this.frames.push({ rolls: [], score: 0, bonus: null });
+    }
+  }
+
+  rollBall() {
+    if (this.ballIsRolled) return;
+    const pinsHit = Math.floor(Math.random() * 11); 
+    for (let i = 0; i < pinsHit; i++) {
+      const randomPinIndex = Math.floor(Math.random() * this.pinsArray.length);
+      this.pinsArray[randomPinIndex].isHit = true;
+    }
+
+    // Update frame values based on pins hit
+    this.updateFrameValues(pinsHit);
+    this.ballIsRolled = true; // Set ballIsRolled flag to true
+  }
+
+  // Function to update frame values after each roll
+  updateFrameValues(pinsHit) {
+    const currentFrame = this.frames[this.currentFrameIndex]; // Get the current frame
+    currentFrame.rolls.push(pinsHit); // Push the number of pins hit in this roll to the rolls array of the current frame
+
+    // Update score
+    currentFrame.score += pinsHit;
+
+    // Check for spare or strike
+    if (currentFrame.rolls.length === 1 && pinsHit === 10) {
+      currentFrame.bonus = "strike";
+    } else if (currentFrame.rolls.length === 2 && currentFrame.score === 10) {
+      currentFrame.bonus = "spare";
+    }
+
+    // Move to the next frame if required
+    if (currentFrame.rolls.length === 2 || currentFrame.bonus === "strike") {
+      this.currentFrameIndex++;
+      this.currentRoll = 1;
+
+      // Reinitialize pinsArray after the end of every frame
+      this.initializePinsArray();
+    } else {
+      this.currentRoll++;
+    }
+  }
+
+  // Function to initialize pinsArray after the end of every frame
+  initializePinsArray() {
+    this.pinsArray = this.generalPins.map((pin, pinId) => {
+      return {pinId: pinId, pinPosition: pin.position, isHit: false}
+    });
+  }
+
+  // Function to setup event listeners
+  setupEventListeners(scene) {
+    // Simulate BabylonJS pointer listener for mouseup event (rolling the ball)
+    scene.onPointerObservable.add((eventData) => {
+      if (eventData.type === BABYLON.PointerEventTypes.POINTERDOWN){
+        this.rollBall();
+      };
+    });
+    console.log(this.frames, this.pinsArray);
+  }
+}
