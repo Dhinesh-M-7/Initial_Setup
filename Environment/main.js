56c5e87225b44d7485d06405915fec1a4ab8c8d1
Dhinesh-M-7 <dhini3322@gmail.com>
2024-02-28 21:49:38 +0530

    Added the backside wall with textures

--- Environment/main.js
+++ Environment/main.js
@@ -15,7 +15,7 @@ async function createScene() {
  
   const camera = new BABYLON.UniversalCamera(
     "camera",
-    new BABYLON.Vector3(0, 30, -110)
+    new BABYLON.Vector3(0, 25, -100)
   );
   camera.setTarget(new BABYLON.Vector3(0, 0, 0));
   camera.attachControl(true);
@@ -23,9 +23,9 @@ async function createScene() {
 
   const light = new BABYLON.HemisphericLight(
     "light",
-    new BABYLON.Vector3(0, 10, -10)
+    new BABYLON.Vector3(0, 10, 0)
   );
-  light.intensity = 0.7;
+  light.intensity = 1.2;
   light.lightmapMode = BABYLON.Light.FALLOFF_PHYSICAL;
 
   scene.enablePhysics(new BABYLON.Vector3(0, -9.8, 0), havokPlugin);
@@ -186,7 +186,7 @@ async function createScene() {
   // // Create a new instance of StartGame with generalPins -- need gui to be added
   // const game = new StartGame(setPins, scene);
 
-  //createAnimations(camera, scene);
+  createAnimations(camera, scene);
   return scene;
 }
 
@@ -362,6 +362,18 @@ const createEnvironment = () => {
   const backWall4Mat = new BABYLON.StandardMaterial();
   backWall4Mat.diffuseColor = new BABYLON.Color4(0, 0, 0, 0);
   backWall4.material = backWall4Mat;
+
+  //Wall at the back of the camera
+  const backWall5 = new BABYLON.MeshBuilder.CreatePlane("plane",{
+    height: 50,
+    width: 100
+  });
+  backWall5.rotation.y = Math.PI;
+  backWall5.position.y = 25;
+  backWall5.position.z = -100;
+  const backWall5Mat = new BABYLON.StandardMaterial();
+  backWall5Mat.diffuseTexture = new BABYLON.Texture("Images/Neon-backsidewall.jpg");
+  backWall5.material = backWall5Mat;
 };
 
 const createAnimations = (camera, scene) => {
@@ -404,7 +416,7 @@ const createAnimations = (camera, scene) => {
 
   movement_keys.push({
     frame: 14 * frameRate,
-    value: new BABYLON.Vector3(0, 30, -110),
+    value: new BABYLON.Vector3(0, 25, -100),
   });
 
   movement.setKeys(movement_keys);
