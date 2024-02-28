191f4b672f756f73ad59043ba113ff5b761182ae
Michael Nickson <michaelnickson2002@gmail.com>
2024-02-28 12:02:09 +0530

    no change

--- Environment/main.js
+++ Environment/main.js
@@ -1,66 +1,86 @@
+
 import * as BABYLON from "@babylonjs/core";
 import "@babylonjs/loaders";
 
+ 
 const canvas = document.getElementById("renderCanvas");
-const engine = new BABYLON.Engine(canvas);
-
+export let engine = new BABYLON.Engine(canvas);
+export let scene;
 async function createScene() {
-  const scene = new BABYLON.Scene(engine);
-
+  scene = new BABYLON.Scene(engine);
+ 
   const havokInstance = await HavokPhysics();
   const havokPlugin = new BABYLON.HavokPlugin(true, havokInstance);
-
+ 
   const camera = new BABYLON.UniversalCamera(
     "camera",
-    new BABYLON.Vector3(0, 10, -90)
+    new BABYLON.Vector3(0, 30, -110)
   );
-  camera.setTarget(new BABYLON.Vector3(0, 0, 60));
-  camera.attachControl(canvas, true);
-
+  camera.setTarget(new BABYLON.Vector3(0, 0, 0));
+  camera.attachControl(true);
+ 
   const light = new BABYLON.HemisphericLight(
     "light",
     new BABYLON.Vector3(0, 10, -10)
   );
   light.intensity = 0.7;
   light.lightmapMode = BABYLON.Light.FALLOFF_PHYSICAL;
-
+ 
+  createBowlingBall();
   createEnvironment();
   createBowlingLane();
   //const groundAggregate = new BABYLON.PhysicsAggregate(ground, BABYLON.PhysicsShapeType.BOX, {mass: 0});
-
+ 
   const result = await BABYLON.SceneLoader.ImportMeshAsync(
     "",
     "Models/",
     "bowling_pin.glb"
   );
   const bowlingPin = result.meshes[1];
-  bowlingPin.scaling = new BABYLON.Vector3(1.5, 1.5, 1.5)
-  bowlingPin.isVisible = false
-
+  bowlingPin.scaling = new BABYLON.Vector3(1.5, 1.5, 1.5);
+  bowlingPin.isVisible = false;
+ 
   const pinPositions = [
-    new BABYLON.Vector3(-7, 0.5, 98),
-    new BABYLON.Vector3(-2.5, 0.5, 98),
-    new BABYLON.Vector3(2.5, 0.5, 98),
-    new BABYLON.Vector3(7, 0.5, 98),
-
+    new BABYLON.Vector3(-10, 0.5, 98),
+    new BABYLON.Vector3(-4, 0.5, 98),
+    new BABYLON.Vector3(4, 0.5, 98),
+    new BABYLON.Vector3(10, 0.5, 98),
+ 
     new BABYLON.Vector3(0, 0.5, 94),
-    new BABYLON.Vector3(-4, 0.5, 94),
-    new BABYLON.Vector3(4, 0.5, 94),
-
-    new BABYLON.Vector3(-2, 0.5, 90),
-    new BABYLON.Vector3(2, 0.5, 90),
-
+    new BABYLON.Vector3(-7, 0.5, 94),
+    new BABYLON.Vector3(7, 0.5, 94),
+ 
+    new BABYLON.Vector3(-3.5, 0.5, 90),
+    new BABYLON.Vector3(3.5, 0.5, 90),
+ 
     new BABYLON.Vector3(0, 0.5, 86),
   ];
-
-  pinPositions.map(function (position, idx) {
+ 
+  const setPins = pinPositions.map(function (position, idx) {
     const pin = new BABYLON.InstancedMesh("pin-" + idx, bowlingPin);
     pin.position = position;
+    return pin;
   });
-
+ 
+  // // Create a new instance of StartGame with generalPins -- need gui to be added
+  // const game = new StartGame(setPins, scene);
+  createAnimations(camera, scene);
   return scene;
 }
-
+ 
+const createBowlingBall = async () => {
+  const result = await BABYLON.SceneLoader.ImportMeshAsync(
+    "",
+    "Models/",
+    "bowling_ball.glb"
+  );
+  const bowling_ball = result.meshes[1];
+  console.log(bowling_ball);
+  bowling_ball.scaling = new BABYLON.Vector3(1, 1, 1);
+  bowling_ball.position.y = 4;
+  bowling_ball.position.z = -67;
+};
+ 
 const createBowlingLane = () => {
   const lane = BABYLON.MeshBuilder.CreateBox("cube", {
     width: 30,
@@ -69,11 +89,11 @@ const createBowlingLane = () => {
   });
   lane.position.y = 0.25;
   lane.position.z = 15;
-
+ 
   const laneMat = new BABYLON.StandardMaterial("lane-material");
-  laneMat.diffuseTexture = new BABYLON.Texture("Images/bowling_floor.jpg");
+  laneMat.diffuseTexture = new BABYLON.Texture("Images/Neon-bowling-floor.jpg");
   lane.material = laneMat;
-
+ 
   const laneLeft = BABYLON.MeshBuilder.CreateBox("cube", {
     width: 1,
     height: 5,
@@ -82,7 +102,11 @@ const createBowlingLane = () => {
   laneLeft.position.x = -15.5;
   laneLeft.position.y = 0.25;
   laneLeft.position.z = 15;
-
+ 
+  const laneLeftMat = new BABYLON.StandardMaterial("lane-material");
+  laneLeftMat.diffuseTexture = new BABYLON.Texture("Images/Neon-floor.jpg");
+  laneLeft.material = laneLeftMat;
+ 
   const laneRight = BABYLON.MeshBuilder.CreateBox("cube", {
     width: 1,
     height: 5,
@@ -91,17 +115,21 @@ const createBowlingLane = () => {
   laneRight.position.x = 15.5;
   laneRight.position.y = 0.25;
   laneRight.position.z = 15;
+ 
+  const laneRightMat = new BABYLON.StandardMaterial("lane-material");
+  laneRightMat.diffuseTexture = new BABYLON.Texture("Images/Neon-floor.jpg");
+  laneRight.material = laneRightMat;
 };
-
+ 
 const createEnvironment = () => {
   const ground = BABYLON.MeshBuilder.CreateGround("ground", {
     width: 100,
     height: 200,
   });
   const groundMat = new BABYLON.StandardMaterial("ground-mat");
-  groundMat.diffuseTexture = new BABYLON.Texture("Images/floor.jpg");
+  groundMat.diffuseTexture = new BABYLON.Texture("Images/Neon-floor.jpg");
   ground.material = groundMat;
-
+ 
   const leftWall = new BABYLON.MeshBuilder.CreatePlane("plane", {
     height: 50,
     width: 200,
@@ -110,9 +138,9 @@ const createEnvironment = () => {
   leftWall.position.y = 25;
   leftWall.rotation.y = -Math.PI / 2;
   const leftWallMat = new BABYLON.StandardMaterial("back-wall-material");
-  leftWallMat.diffuseTexture = new BABYLON.Texture("Images/floor.jpg");
+  leftWallMat.diffuseTexture = new BABYLON.Texture("Images/Neon-sidewall.jpg");
   leftWall.material = leftWallMat;
-
+ 
   const rightWall = new BABYLON.MeshBuilder.CreatePlane("plane", {
     height: 50,
     width: 200,
@@ -121,43 +149,139 @@ const createEnvironment = () => {
   rightWall.position.y = 25;
   rightWall.rotation.y = Math.PI / 2;
   const rightWallMat = new BABYLON.StandardMaterial("back-wall-material");
-  rightWallMat.diffuseTexture = new BABYLON.Texture("Images/floor.jpg");
+  rightWallMat.diffuseTexture = new BABYLON.Texture("Images/Neon-sidewall.jpg");
   rightWall.material = rightWallMat;
-
+ 
   //Wall right behind the pins(left as a black screen)
   const backWall1 = new BABYLON.MeshBuilder.CreatePlane("plane", {
-    height: 10,
+    height: 15,
     width: 30,
   });
-  backWall1.position.y = 5;
+  backWall1.position.y = 7.5;
   backWall1.position.z = 100;
-
+  const backWall1Mat = new BABYLON.StandardMaterial();
+  backWall1Mat.diffuseColor = new BABYLON.Color4(0, 0, 0, 0);
+  backWall1.material = backWall1Mat;
+ 
   //Wall where the texture has to be applied
   const backWall2 = new BABYLON.MeshBuilder.CreatePlane("plane", {
-    height: 40,
+    height: 35,
     width: 100,
   });
-  backWall2.position.y = 30;
+  backWall2.position.y = 32.5;
   backWall2.position.z = 100;
-
+  const backWall2Mat = new BABYLON.StandardMaterial();
+  backWall2Mat.diffuseTexture = new BABYLON.Texture("Images/Backwall.jpg");
+  backWall2.material = backWall2Mat;
+ 
   //Walls on sides of backWall1(No texture needed only color)
   const backWall3 = new BABYLON.MeshBuilder.CreatePlane("plane", {
-    height: 10,
-    width: 40,
+    height: 15,
+    width: 35,
   });
-  backWall3.position.x = -30;
-  backWall3.position.y = 5;
+  backWall3.position.x = -32.5;
+  backWall3.position.y = 7.5;
   backWall3.position.z = 100;
-
+  const backWall3Mat = new BABYLON.StandardMaterial();
+  backWall3Mat.diffuseColor = new BABYLON.Color4(0, 0, 0, 0);
+  backWall3.material = backWall3Mat;
+ 
   const backWall4 = new BABYLON.MeshBuilder.CreatePlane("plane", {
-    height: 10,
-    width: 40,
+    height: 15,
+    width: 35,
   });
-  backWall4.position.x = 30;
-  backWall4.position.y = 5;
+  backWall4.position.x = 32.5;
+  backWall4.position.y = 7.5;
   backWall4.position.z = 100;
+  const backWall4Mat = new BABYLON.StandardMaterial();
+  backWall4Mat.diffuseColor = new BABYLON.Color4(0, 0, 0, 0);
+  backWall4.material = backWall4Mat;
 };
+ 
+const createAnimations = (camera, scene) => {
+  const frameRate = 60;
+ 
+  const movement = new BABYLON.Animation(
+    "movement",
+    "position",
+    frameRate,
+    BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
+    BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT,
+    true
+  );
+  const movement_keys = [];
+ 
+  movement_keys.push({
+    frame: 0,
+    value: new BABYLON.Vector3(0, 20, -120),
+  });
+ 
+  movement_keys.push({
+    frame: 1 * frameRate,
+    value: new BABYLON.Vector3(0, 10, -100),
+  });
+ 
+  movement_keys.push({
+    frame: 5 * frameRate,
+    value: new BABYLON.Vector3(0, 10, 60),
+  });
+ 
+  movement_keys.push({
+    frame: 6 * frameRate,
+    value: new BABYLON.Vector3(0, 30, 50),
+  });
+ 
+  movement_keys.push({
+    frame: 13 * frameRate,
+    value: new BABYLON.Vector3(0, 30, -100),
+  });
+ 
+  movement_keys.push({
+    frame: 14 * frameRate,
+    value: new BABYLON.Vector3(0, 30, -110),
+  });
+ 
+  movement.setKeys(movement_keys);
+ 
+  const rotation = new BABYLON.Animation(
+    "rotate",
+    "rotation.y",
+    frameRate,
+    BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+    BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT,
+    true
+  );
+  const rotate_keys = [];
+ 
+  rotate_keys.push({
+    frame: 0,
+    value: 0,
+  });
+  rotate_keys.push({
+    frame: 5 * frameRate,
+    value: 0,
+  });
+  rotate_keys.push({
+    frame: 9 * frameRate,
+    value: Math.PI,
+  });
+  rotate_keys.push({
+    frame: 13 * frameRate,
+    value: 2 * Math.PI,
+  });
+ 
+  rotation.setKeys(rotate_keys);
+ 
+  scene.beginDirectAnimation(
+    camera,
+    [movement, rotation],
+    0,
+    14 * frameRate,
+    false,
 
+  );
+};
+ 
 createScene().then((scene) => {
   engine.runRenderLoop(function () {
     if (scene) {
@@ -168,3 +292,4 @@ createScene().then((scene) => {
 window.addEventListener("resize", function () {
   engine.resize();
 });
+ 
