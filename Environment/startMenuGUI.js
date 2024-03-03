24f617140005c94cb8fd4c277985172a2c2ab4e6
Dhinesh-M-7 <dhini3322@gmail.com>
2024-03-03 15:10:11 +0530

    Made the infoGUI responsive

--- Environment/startMenuGUI.js
+++ Environment/startMenuGUI.js
@@ -80,8 +80,11 @@ const handleExitGame = () => {
   customWindow.close();
 };
 
-const handleInfo = (scene) => {
-  infoGUI(scene);
+const handleInfo = (startPlane, infoPlane, exitPlane, scene, game) => {
+  startPlane.dispose();
+  infoPlane.dispose();
+  exitPlane.dispose();
+  infoGUI(scene, game);
 };
 
 export function startMenuGUI(scene, game) {
@@ -98,7 +101,7 @@ export function startMenuGUI(scene, game) {
   });
 
   infoButton.onPointerUpObservable.add(function () {
-    handleInfo(scene);
+    handleInfo(startPlane, infoPlane, exitPlane, scene, game);
   });
   return { startGameButton, infoButton, exitGameButton };
 }
